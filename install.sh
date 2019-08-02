700a74c4d5356560ca667a053cf791e1045792ac
Lars Kreisz <kraiz@users.noreply.github.com>
2019-08-02 12:34:05 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,6 +18,17 @@ export PATH="~/.local/bin:~/.npm-packages/bin:$PATH"
 export LS_COLORS=$LS_COLORS:'di=0;36:'
 export LSCOLORS=gxfxcxdxbxegedabagacad
 
+# history
+export HISTFILE=~/.zsh_history
+export HISTSIZE=10000000
+export SAVEHIST=10000000
+setopt HIST_VERIFY
+setopt EXTENDED_HISTORY      # save each command's beginning timestamp and the duration to the history file
+setopt HIST_IGNORE_ALL_DUPS
+setopt HIST_REDUCE_BLANKS
+setopt INC_APPEND_HISTORY    # this is default, but set for share_history
+setopt SHARE_HISTORY         # Share history file amongst all Zsh sessions
+
 # tools
 alias v='vim'
 alias d='docker'
